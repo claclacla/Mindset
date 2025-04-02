5c700c880a972bfb3c1395b56798bebf126c0203
Admin <admin@MacBook-Pro-di-Admin.local>
2025-04-02 11:13:07 +0200

    Removed a comment from repositories/store.ts

--- src/app/repositories/store.ts
+++ src/app/repositories/store.ts
@@ -1,18 +1,3 @@
-/*import { configureStore } from "@reduxjs/toolkit";
-import trainingSessionsReducer from "./trainingSessions/slice";
-import authenticationReducer from "./authentication/slice";
-
-export const store = configureStore({
-    reducer: {
-        trainingSessions: trainingSessionsReducer,
-        authentication: authenticationReducer
-    },
-});
-
-export type RootState = ReturnType<typeof store.getState>;
-export type AppDispatch = typeof store.dispatch;
-*/
-
 import { combineReducers, configureStore } from '@reduxjs/toolkit'
 import { persistStore, persistReducer } from 'redux-persist'
 import storage from 'redux-persist/lib/storage'
